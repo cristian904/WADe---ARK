9507279fe7c0a45c7521a10cfb846fb2d5316f07
cristian9004 <cristian.padurariu94@gmail.com>
2017-12-30 13:22:23 +0200

    created components and routing

---
+++ ARK-client/src/app/art/art-detail/art-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-art-detail',
+    templateUrl: './art-detail.component.html',
+    styleUrls: ['./art-detail.component.scss']
+})
+export class ArtDetailComponent{
+
+}
