af23887a4eaa77ef1edf308b526b4ef242dfcb41
Dragos Silion <silidragos@gmail.com>
2018-01-31 21:27:34 +0200

    small fix

--- ARK-client/src/app/constants.ts
+++ ARK-client/src/app/constants.ts
@@ -1,4 +1,4 @@
 export const APP_CONSTANTS = {
-    ENDPOINT: "https://werit.me:8443/server-0.0.1-SNAPSHOT"
+    ENDPOINT: "http://46.101.178.48:8080/server-0.0.1-SNAPSHOT"
     // ENDPOINT: "http://192.168.0.19:8080/server"
 }
